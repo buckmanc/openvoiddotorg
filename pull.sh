90dd7a91d5e155488850e1d84cb4bca41a0ce1c6
Charles Buckman <buckmanc@gmail.com>
2026-03-05 23:50:47 -0600

    install dependencies during pull

--- pull.sh
+++ pull.sh
@@ -1,6 +1,40 @@
 #!/usr/bin/env bash
 
+set -e
+
+ffmpegLocArgs=()
+
+if ! type ffmpeg > /dev/null 2>&1
+then
+	curl -Ls 'https://ffbinaries.com/api/v1/version/latest' | jq -r '.bin."linux-64"'.ffmpeg | xargs -n 1 curl -O
+	zipPath="$(find . -type f -iname 'ffmpeg*.zip' -print -quit)"
+	unzip "$zipPath" -d .
+	ffmpegLocArgs=(--ffmpeg-location ".")
+
+	rm "$zipPath"
+fi
+
+if ! type yt-dlp > /dev/null 2>&1
+then
+	pip install yt-dlp
+fi
+
 url="https://youtu.be/dQw4w9WgXcQ"
-yt-dlp -S "res:720" --no-embed-thumbnail -t sleep "$url" -o "openroll_temp.webm"
-ffmpeg -i openroll_temp.webm -vf 'crop=ih/3*4:ih' openroll.webm
+yt-dlp \
+	-S "res:720" \
+	--no-embed-thumbnail \
+	-t sleep \
+	"$url" \
+	-o "openroll_temp.webm" \
+	--write-subs \
+	--sub-format 'vtt' \
+	--sub-langs 'en' \
+	"${ffmpegLocArgs[@]}"
+
+# crop out the black bars
+ffmpeg -i openroll_temp.webm -vf 'crop=ih/3*4:ih' openroll_intro.webm
+mv openroll_temp.en.vtt openroll_intro.en.vtt
+
+# clean up
 rm openroll_temp.webm
+rm -f ./ffmpeg
